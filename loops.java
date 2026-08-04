87db11b9dab544e227660751d0c324b9ea17de93
prototype7421-gif <prototype7421@gmail.com>
2026-08-04 23:03:16 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,13 @@
+public class loops{
+  public static void main(String[] args){
+     int n=4;
+     int sum =0;
+      for( int i=0;i<=n;i++){
+        sum = sum + i;
+        System.out.println(sum);
+        
+        }
+      
+     
+  }
+}
